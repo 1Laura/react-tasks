d02751287b5d8415ab326c9a91424523d14c1a82
Laura <baltrukone@gmail.com>
2024-12-17 08:24:45 +0200

    cookie clicker by me

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import {use, useState} from "react";
+import {useState} from "react";
 
 function App() {
     const [count, setCount] = useState(0);
     const [getRotateCookie, setRotateCookie] = useState(0);
 
     const rotateCookie = () => {
-        setCount(count + 1);
+        setCount(count + 5);
         setRotateCookie(getRotateCookie + 5)
     };
 
     return (
         <div className="container">
             <h1>🍪 Cookie Counter</h1>
-            <div className={"cookie "} onClick={rotateCookie} style={{transform: `rotate(${getRotateCookie}deg)`}}
-            >
-                🍪
+            <div className="cookie" onClick={rotateCookie} style={{transform: `rotate(${getRotateCookie}deg)`}}
+            >🍪
             </div>
             <p style={{fontSize: "2em"}}>{count}</p>
 
