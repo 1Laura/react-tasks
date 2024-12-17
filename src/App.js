227c7694aa66b2e34e1808354581d9deb27cf8e4
Laura <baltrukone@gmail.com>
2024-12-17 09:32:25 +0200

    fixed progress bar with color

--- src/App.js
+++ src/App.js
@@ -5,40 +5,20 @@ function App() {
     const [getColor, setColor] = useState("#308a11");
     const [getProgressWidth, setProgressWidth] = useState(0);
 
-    function updateProgress10() {
+    function updateProgress(num) {
         if (getProgressWidth > 800) {
             return;
         }
-        setProgressWidth(getProgressWidth + 10);
-        if (getProgressWidth > 200) {
+        setProgressWidth(getProgressWidth + num);
+        if (getProgressWidth > 200 && getProgressWidth < 400) {
             setColor("#f1d218");
         }
-        if (getProgressWidth > 400) {
+        if (getProgressWidth > 400 && getProgressWidth < 600) {
             setColor("#f48a1f");
         }
         if (getProgressWidth > 600) {
             setColor("#f41f1f");
         }
-    }
-
-    function updateProgress30() {
-        if (getProgressWidth > 800) {
-            return;
-        }
-        setProgressWidth(getProgressWidth + 30);
-        if (getProgressWidth > 200) {
-            setColor("#f1d218");
-        }
-        if (getProgressWidth > 400) {
-            setColor("#f48a1f");
-        }
-        if (getProgressWidth > 600) {
-            setColor("#f41f1f");
-        }
-        if (getProgressWidth >= 800) {
-            setColor("#f41f1f");
-        }
-
     }
 
     return (
@@ -50,11 +30,11 @@ function App() {
 
             <div className="buttons">
                 <button onClick={() => {
-                    updateProgress10()
+                    updateProgress(10)
                 }}>1 per click
                 </button>
                 <button onClick={() => {
-                    updateProgress30()
+                    updateProgress(30)
                 }}>3 per click
                 </button>
             </div>
