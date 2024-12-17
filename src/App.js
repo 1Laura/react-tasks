e8105b74f0824827d95660b7e4fc0773bb4160c7
Laura <baltrukone@gmail.com>
2024-12-17 09:10:58 +0200

    progress bar with color

--- src/App.js
+++ src/App.js
@@ -2,53 +2,66 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
+    const [getColor, setColor] = useState("#308a11");
+    const [getProgressWidth, setProgressWidth] = useState(0);
 
-    const [getCounter, setCounter] = useState(0)
-    const [perClick, setPerClick] = useState(1)
-    const [bgColor, setBgColor] = useState("white")
-    const [image, setImage] = useState("https://static.vecteezy.com/system/resources/thumbnails/044/279/881/small/chocolate-chips-on-cookie-on-transparent-background-png.png")
-
-    function addCounter() {
-        setCounter(getCounter + perClick)
+    function updateProgress10() {
+        if (getProgressWidth > 800) {
+            return;
+        }
+        setProgressWidth(getProgressWidth + 10);
+        if (getProgressWidth > 200) {
+            setColor("#f1d218");
+        }
+        if (getProgressWidth > 400) {
+            setColor("#f48a1f");
+        }
+        if (getProgressWidth > 600) {
+            setColor("#f41f1f");
+        }
     }
 
-    function buyUpgrade(upgradeNum, pointsCost) {
-        if (getCounter < pointsCost) return
-        setCounter(getCounter - pointsCost)
+    function updateProgress30() {
+        if (getProgressWidth > 800) {
+            return;
+        }
+        setProgressWidth(getProgressWidth + 30);
+        if (getProgressWidth > 200) {
+            setColor("#f1d218");
+        }
+        if (getProgressWidth > 400) {
+            setColor("#f48a1f");
+        }
+        if (getProgressWidth > 600) {
+            setColor("#f41f1f");
+        }
+        if (getProgressWidth >= 800) {
+            setColor("#f41f1f");
+        }
 
-        if (upgradeNum === 1) {
-            setPerClick(2)
-        }
-        if (upgradeNum === 2) {
-            setPerClick(5)
-        }
-        if (upgradeNum === 3) {
-            setBgColor("gray")
-        }
-        if (upgradeNum === 4) {
-            setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG8D47zJvBKG-h-9SxUXao_FFBJlzi2B4d-Q&s")
-        }
     }
 
     return (
 
-        <div className="d-flex j-center gap20" style={{backgroundColor: bgColor}}>
-            <div className="random ">
-                <img onClick={addCounter}
-                     src={image}
-                     alt=""/>
-                <h2>Counter: {getCounter}</h2>
+        <div className="container">
+            <div className="progress-bar">
+                <div className="progress" style={{backgroundColor: getColor, width: getProgressWidth}}></div>
             </div>
 
-            <div className="d-flex direction-col">
-                <button onClick={() => buyUpgrade(1, 10)}>2 per click (10p)</button>
-                <button onClick={() => buyUpgrade(2, 20)}>5 per click (20p)</button>
-                <button onClick={() => buyUpgrade(3, 50)}>change bg color (50p)</button>
-                <button onClick={() => buyUpgrade(4, 50)}>Change cookie image (50p)</button>
+            <div className="buttons">
+                <button onClick={() => {
+                    updateProgress10()
+                }}>1 per click
+                </button>
+                <button onClick={() => {
+                    updateProgress30()
+                }}>3 per click
+                </button>
             </div>
         </div>
 
-    );
+    )
+        ;
 
 }
 
