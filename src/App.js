eef557a65fbef3277252a7809b7102bd3088885e
Laura <baltrukone@gmail.com>
2025-01-07 20:18:00 +0200

    Create record from input, open this record and update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
-import {useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 
 function App() {
+    // record part start
+    const inputTextRef = useRef("");
+    const updateTextRef = useRef("");
+    const [recordValueArray, setRecordValueArray] = useState([]);
+    const [recordSelected, setRecordSelected] = useState(-1)
+
+    function createRecord() {
+        const newRecord = {
+            value: inputTextRef.current.value,
+            time: new Date().toLocaleTimeString(),
+        }
+        const recordValueArrayCopy = [...recordValueArray];
+        recordValueArrayCopy.push(newRecord);
+        setRecordValueArray(recordValueArrayCopy)
+        inputTextRef.current.value = "";
+    }
+
+    function updateRecord(index) {
+        const updateRecord = {
+            value: updateTextRef.current.value,
+            time: new Date().toLocaleTimeString(),
+        }
+        // atnaujinu irasa
+        const recordValueArrayCopy = [...recordValueArray];
+        // perduodu indexa iraso ir jo reiksme
+        recordValueArrayCopy[index] = updateRecord;
+        setRecordValueArray(recordValueArrayCopy);
+        updateTextRef.current.value = "";
+    }
+
+    // move box part
     const [array1, setArray1] = useState(["purple", "pink", "coral", "violet"]);
     const [array2, setArray2] = useState([]);
 
@@ -26,25 +57,57 @@ function App() {
 
     return (
         <div className="container">
-            <div className="box">
-                {array1.map((color1, index1) =>
-                    <div key={index1}
-                         className="color"
-                         style={{backgroundColor: color1}}
-                         onClick={() => colorMoveToBox2(index1)}>
+            <div className="record-container">
+                <div className="record-block">
+                    {recordValueArray.map((record, index) =>
+                        <div className="record-render d-flex" key={index}>
+                            <div className="record-render-text-time">
+                                <p>{record.value}</p>
+                                <p>{record.time}</p>
+                            </div>
+                            <button onClick={() => setRecordSelected(index)}>Edit</button>
+                        </div>
+                    )}
+                    <div className="record-input d-flex">
+                        <input type="text"
+                               placeholder="Enter text"
+                               ref={inputTextRef}/>
+                        <button onClick={createRecord}>Create</button>
                     </div>
-                )}
-            </div>
-            <div className="box">
-                {array2.map((color2, index2) =>
-                    <div key={index2}
-                         className="color"
-                         style={{backgroundColor: color2}}
-                         onClick={() => colorMoveToBox1(index2)}>
+                </div>
+
+                {recordSelected >= 0 &&
+                    <div className="record-block record-block-edit">
+                        <p>{recordValueArray[recordSelected].value}</p>
+                        <input type="text" placeholder="Update text"
+                               ref={updateTextRef}/>
+                        <button onClick={() => updateRecord(recordSelected)}>Update</button>
                     </div>
-                )}
+                }
             </div>
 
+            <hr/>
+
+            <div className="box-move-container">
+                <div className="box">
+                    {array1.map((color1, index1) =>
+                        <div key={index1}
+                             className="color"
+                             style={{backgroundColor: color1}}
+                             onClick={() => colorMoveToBox2(index1)}>
+                        </div>
+                    )}
+                </div>
+                <div className="box">
+                    {array2.map((color2, index2) =>
+                        <div key={index2}
+                             className="color"
+                             style={{backgroundColor: color2}}
+                             onClick={() => colorMoveToBox1(index2)}>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 }
