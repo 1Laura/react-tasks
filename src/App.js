24124f7ce918a8043ab1311b94c27e422496364d
Laura <baltrukone@gmail.com>
2025-01-16 13:37:34 +0200

    posts render with different(updated) title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import SinglePost from "./components/SinglePost";
 
 function App() {
     const [posts, setPosts] = useState([])
-
+    const [title, setTitle] = useState(""); // State for the title
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts")
@@ -13,13 +13,18 @@ function App() {
             .then(data => {
                 setPosts(data)
             })
-    },[])
+    }, [])
+
+    function updateTitle(data) {
+        setTitle(data);
+    }
 
     return (
         <div className="container">
+            <h2>{title}</h2>
             <div className="d-flex flex-wrap gap-1">
                 {posts.map((post, index) =>
-                    <SinglePost key={index} postInfo={post}/>
+                    <SinglePost key={index} postInfo={post} update={updateTitle}/>
                 )}
             </div>
 
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,9 +1,14 @@
-const SinglePost = ({postInfo}) => {
+const SinglePost = ({postInfo, update}) => {
+    function updateItem() {
+        update(postInfo.title)//galima atlikti logika
+    }
 
     return (
         <div className="card p-2">
             <h5>{postInfo.title}</h5>
             <p>{postInfo.body}</p>
+            {/*<button onClick={()=>update(postInfo.title)}>Click</button>*/}
+            <button onClick={updateItem}>Click</button>
         </div>
     );
 };
