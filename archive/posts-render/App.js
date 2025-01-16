598abb1656308a11401ce2035331196269311f7f
Laura <baltrukone@gmail.com>
2025-01-16 13:37:23 +0200

    posts render with different(updated) title

--- archive/posts-render/App.js
+++ archive/posts-render/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import { useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import SinglePost from "./components/SinglePost";
 
 function App() {
@@ -13,13 +13,18 @@ function App() {
             .then(data => {
                 setPosts(data)
             })
-    },[])
+    }, [])
+
+    function updateTitle() {
+        console.log("asdasd")
+    }
 
     return (
         <div className="container">
+            <h2>This is page title</h2>
             <div className="d-flex flex-wrap gap-1">
                 {posts.map((post, index) =>
-                    <SinglePost key={index} postInfo={post}/>
+                    <SinglePost key={index} postInfo={post} update={updateTitle}/>
                 )}
             </div>
 
