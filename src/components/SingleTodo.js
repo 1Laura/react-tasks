e005fc5a65adb8a0d1f8d8021b76dc6a4e2d7643
Laura <baltrukone@gmail.com>
2025-01-27 22:07:29 +0200

    single todos fixed

--- src/components/SingleTodo.js
+++ src/components/SingleTodo.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 const SingleTodo = ({userTodosInfo}) => {
-
+    const backgroundColor = userTodosInfo.completed ? "lightgreen" : "#f50000b0";
 
     return (
-
-
-        <div style={{backgroundColor: {userTodosInfo.completed&&}}  className="card flex-wrap m-2 p-2">
+        <div style={{backgroundColor}} className="card flex-wrap m-2 p-2">
             <h5>{userTodosInfo.title}</h5>
         </div>
     );
