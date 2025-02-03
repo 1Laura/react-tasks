3cef610680579467db29b8a8e6361d639074664b
Laura <baltrukone@gmail.com>
2025-02-03 12:39:59 +0200

    Users list done

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -4,12 +4,13 @@ import useUserStore from "../store/main";
 
 const Toolbar = () => {
     const {currentUser} = useUserStore();
+
     return (
         <div className={"container-fluid"}>
             <Link to="/" className="p-2">Login</Link>
             <Link to="/register" className="p-2">Register</Link>
             {currentUser && <Link to="/profile" className="p-2">Profile</Link>}
-
+            {currentUser && <Link to="/userslist" className="p-2">Users list</Link>}
 
         </div>
     );
--- src/pages/AllUsersPage.js
+++ src/pages/AllUsersPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import useUserStore from "../store/main";
 
 const AllUsersPage = () => {
+    const {users} = useUserStore();
+
     return (
-        <div className={"container"}>
-            //listas registruotu useriu
-            <img src="" alt=""/>
-            <p>Username</p>
+        <div className="container d-flex">
+            {users.map((user, index) =>
+                <div className="user-card flex-wrap" key={index}>
+                    <img src={user.avatar} alt=""/>
+                    <p>{user.username}</p>
+                </div>
+            )}
+
         </div>
     );
 };
